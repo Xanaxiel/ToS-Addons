e84e83e718a870299e1d5c5ad97c3dd120055348
Xanaxiel <cyril.pangilinan@gmail.com>
2018-05-20 03:31:21 +0800

    Refactoring

--- addons/tooltiphelper/tooltiphelper.lua
+++ addons/tooltiphelper/tooltiphelper.lua
@@ -4,7 +4,7 @@ _G['ADDONS'] = _G['ADDONS'] or {};
 TTH = _G["ADDONS"]["TOOLTIPHELPER"] or {};
 
 TTH.configFile = '../addons/tooltiphelper/tooltiphelper.json'
-TTH.recipeFile = "\tooltiphelper\recipe_puzzle.xml";
+TTH.recipeFile = "../addons/tooltiphelper/recipe_puzzle.xml";
 
 TTH.config = {
     showCollectionCustomTooltips = true,
